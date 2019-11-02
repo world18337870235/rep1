7189394aaf0b797126e80a9013ec6f7e0fff4998
world18337870235 <1642122910@qq.com>
2019-11-03 02:12:09 +0800

    bxg

---
+++ bxg/pages/index/index.js
@@ -0,0 +1,56 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    headlist:[
+      {id:1,lujing:"https://m.boxuegu.com/web/images/index/course.png",comment:"就业课"},
+      {id:2,lujing:"https://m.boxuegu.com/web/images/index/mircoCourse.png",comment:"提升课"},
+      {id:3,lujing:"https://m.boxuegu.com/web/images/index/freeMicroCourse.png",comment:"免费课"},
+      {id:4,lujing:"https://m.boxuegu.com/web/images/index/courseType.png",comment:"课程分类"},
+    ],
+    secondList:[
+      {id:1,img:"https://file.boxuegu.com/f6c878722f394a7c9bd12c0319f00037.png",tittle:"JavaEE",content:"JavaEE是一个开放的、基于标准的平台",stydu:"1784人学习",price:"￥14980.00"},
+      {id:2,img:"https://file.boxuegu.com/e19c53c6c1fa468aad6e11cb61d81b9a.png",tittle:"Python+人工智能",content:"最有前景的开发职位",stydu:"877人学习",price:"￥14980.00"},
+      {id:3,img:"https://file.boxuegu.com/3940be1c755a44eeb8e2e0aaf984756e.png",tittle:"前端与移动开发",content:"用户界面的构筑者",stydu:"1230人学习",price:"￥14980.00"},
+      {id:4,img:"https://file.boxuegu.com/6c0ffe21a06d421d90be8a0061c14c1a.png",tittle:"软件测试",content:"",stydu:"189人学习",price:"￥12980.00"}
+    ],
+    thirdSmalllist:[
+      [{id:1,img:"https://file.boxuegu.com/e6432db4d06c42b9aca7aede22ba9437.jpg",title:"数据分析课程",study:"20人学习",price:"￥222"},{id:2,img:"https://file.boxuegu.com/989b9bd2a12d471b8e7579b7182d08b6.png",title:"数据库优化",study:"20人学习",price:"￥222"}  ],
+      [{id:3,img:"https://file.boxuegu.com/df25a3e7031a48d39a896d4e38d3d3d5.png",title:"Java并发",study:"20人学习",price:"￥222"},{id:4,img:"https://file.boxuegu.com/854b60de9d6742dbb29a47370b7c8650.png",title:"对抗网络",study:"20人学习",price:"￥222"}  ],
+      [{id:5,img:"https://file.boxuegu.com/746a8e78655a4b3ca97ccad5390fbc4b.png",title:"电信数据分析",study:"20人学习",price:"￥222"},{id:6,img:"https://file.boxuegu.com/90bb08a6bacd49baa5492c5eaa94c060.jpg",title:"游戏AI",study:"20人学习",price:"￥222"}  ],
+    ],
+    fourthSmalllist:[
+      [{id:1,img:"https://file.boxuegu.com/74773be265b840b3af9da88f6f42841d.jpg",title:"数据分析课程",study:"20人学习",price:"￥222"},{id:2,img:"https://file.boxuegu.com/7e1535f414a64d3fa4d60e7c8a84cca3.jpg",title:"数据库优化",study:"20人学习",price:"￥222"}  ],
+      [{id:3,img:"https://file.boxuegu.com/3ed09c2ae4ea4542add4ac8b098c9535.jpg",title:"Java并发",study:"20人学习",price:"￥222"},{id:4,img:"https://file.boxuegu.com/6a0330c2237b4ed3af2a2703cc2f1419.jpg",title:"对抗网络",study:"20人学习",price:"￥222"}  ],
+      [{id:5,img:"https://file.boxuegu.com/ccc47386e2b048658f944d0db943321c.jpg",title:"电信数据分析",study:"20人学习",price:"￥222"},{id:6,img:"https://file.boxuegu.com/49b20b70e42e446cafc60f9c6fd98d84.jpg",title:"游戏AI",study:"20人学习",price:"￥222"}  ],
+    ]
+
+  },
+  
+
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady () {
+
+  },
+
+ 
+
+ 
+
+ 
+})
